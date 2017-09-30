77f0e6db04f9ff46c62b5b5489d1d3a7aa5b9ca8
Ken Fukuyama <kenfdev@gmail.com>
2017-09-30 15:15:43 +0900

    testing ImageUuid

--- handlers/deploy.go
+++ handlers/deploy.go
@@ -84,6 +84,7 @@ func makeServiceSpec(request requests.CreateFunctionRequest) *client.Service {
 	launchConfig := &client.LaunchConfig{
 		Environment: envVars,
 		Image:       request.Image,
+		ImageUuid:   "docker:" + request.Image,
 		Labels:      labels,
 	}
 
